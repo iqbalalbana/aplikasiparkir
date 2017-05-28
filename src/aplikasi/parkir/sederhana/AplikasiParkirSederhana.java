4d4352ca05c5225ca8d5e54aae0d8a5d1fa5ce22
Iqbal Albana <iqbalalbana16@gmail.com>
2017-05-28 09:12:55 +0700

    - finish

---
+++ src/aplikasi/parkir/sederhana/AplikasiParkirSederhana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.parkir.sederhana;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class AplikasiParkirSederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_Aplikasi_Parkir Laund = new Frame_Aplikasi_Parkir();
+        Laund.setVisible(true);
+    }
+}
